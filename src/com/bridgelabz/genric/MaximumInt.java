f6718252e2de57475d17b02c5e7b175f75afa44f
shekhar <shekharjha7417870578@gmail.com>
2022-08-18 22:11:05 +0530

    Added uc1 : maximum  between 3 Integers

---
+++ src/com/bridgelabz/genric/MaximumInt.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.genric;
+
+import java.util.Scanner;
+
+public class MaximumInt {
+    public static void main(String[] args) {
+
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Enter 3 Integer number :");
+
+        Integer num1 = sc1.nextInt();
+        Integer num2 = sc1.nextInt();
+        Integer num3 = sc1.nextInt();
+
+        MaximumInt.findingMaxNumber(num1, num2, num3);
+    }
+
+    public static Integer findingMaxNumber(Integer num1, Integer num2, Integer num3) {
+        int max = num1;
+        if (num2.compareTo(max) > 0) {
+            max = num2;
+        }
+        if (num3.compareTo(max) > 0) {
+            max = num3;
+        }
+        System.out.println(" Max number is : " + max);
+        return max;
+
+    }
+
+}
+
